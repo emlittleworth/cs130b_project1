b11af2309b8ccdd861f3d307d2a0590b3baa103e
Emily Littleworth <eml719@gmail.com>
2014-04-27 22:38:42 -0700

    Updated main file to save groups with pointers Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@
 int main() {
     int n, m, k, algorithm;
     int first, second, weight;
+    int group_size, vertex;
     cin >> n;
     cin >> m;
     cin >> k;
     cin >> algorithm;
 
+    // create adjacency list
     VertexItem* p = new VertexItem[n];
     AdjacencyList Graph(n);
     Graph.set_array_ptr(p);
@@ -21,6 +23,7 @@ int main() {
     }
 
     // insert each edge (twice for each edge)
+    // into adjacency list
     for (int i = 0; i < m; i++) {
         cin >> first;
         cin >> second;
@@ -29,7 +32,30 @@ int main() {
         Graph.insert(second, first, weight);
     }
 
+    // store groups
+    // create array of k pointers
+    int** Groups = new int*[k];
+
+    // create array for each group
+    for (int i = 0; i < k; i++) {
+        cin >> group_size;
+        int *temp = new int[group_size];
+        Groups[i] = temp;
+        Groups[i][0] = group_size;
+        for (int j = 1; j <= Groups[i][0]; j++) {
+            cin >> vertex;
+            Groups[i][j] = vertex;
+        }
+    }
+
     Graph.print_list();
+
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j <= Groups[i][0]; j++) {
+            cout << Groups[i][j] << " ";
+        }
+        cout << "\n";
+    }
 
 
 
